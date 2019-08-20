62ac2e5d99a692669cc738996c8d997de74e174c
Ronah143 <53499750+Ronah143@users.noreply.github.com>
2019-08-20 23:14:18 +0800

    Add files via upload

---
+++ photo 1.2.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Photo 1</title>
+
+<Style type='text/css' >body{background-color:rgb(0,0,0);background-image:url(http://www.zingerbugimages.com/backgrounds/brown_ornate_floral_wallpaper_tileable.jpg);background-position:top left;background-repeat:repeat;background-attachment:fixed;}</Style>
+</head>
+<body>
+
+
+<header>
+    <h1><center><font size= "15">Emperio, RJ's Photo Gallery</font></center></h1>
+  </header>
+
+<center><img src="photo 1.jpeg" width="500" height="600"></center>
+
+
+ <p align="right"><font size= "5"><a href="Photo 2.2.php">Next</a></font></p>
+<p align="left"><font size= "5"><a href="Home.2.php">Go Back to Home</a></font></p>
+
+</body>
+</html>
